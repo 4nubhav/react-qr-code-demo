d0b5003e6caafb531bfd3d664db914fde07cbdcb
Anubhav <anubhavdas54@gmail.com>
2020-08-12 20:04:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import QRCode from 'react-qr-code'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+
+    return (
+        <div className='wrapper centered'>
+            <div className='flex-container'>
+                <div className='box form'>
+                    <input type='text'
+                           placeholder='First Name'
+                           value={firstName}
+                           onChange={(e) => setFirstName(e.target.value)}/>
+                    <input type='text'
+                           placeholder='Last Name'
+                           value={lastName}
+                           onChange={(e) => setLastName(e.target.value)}/>
+                </div>
+                <div className='box'>
+                    <QRCode value={`${firstName} ${lastName}`}/>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
